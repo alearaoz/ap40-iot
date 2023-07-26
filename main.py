58b6b19bc25455fb282d2824ca4fff77ab355b6d
Jose Alejandro Araoz <jose.araoz@rappi.com>
2023-07-26 01:32:48 -0300

    Capitulo 2 - Tarea 2

---
+++ main.py
@@ -0,0 +1,20 @@
+from machine import Pin
+import time
+
+print("esperando pulsador")
+
+sw = Pin(23, Pin.IN)
+led = Pin(2, Pin.OUT)
+
+contador = 0
+while True:
+    if sw.value():
+        contador += 1
+        print(contador)
+        if contador >= 3:
+            print("pulsacion larga")
+    else:
+        if contador >= 1 and contador < 3:
+            print("pulsacion corta")
+        contador = 0
+    time.sleep_ms(200)
